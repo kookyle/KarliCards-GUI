fee76c8c0dadbb4cf31ac8bdb5c7bc31a9e7a59f
Skype <moomine@live.com>
2018-01-25 21:11:55 +0800

    Add Cards Collection

---
+++ CardLib/Cards.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CardLib
+{
+    public class Cards : CollectionBase
+    {
+        public void Add(Card newCard)
+        {
+            List.Add(newCard);
+        }
+
+        public void Remove(Card oldCard)
+        {
+            List.Remove(oldCard);
+        }
+
+        public Card this[int cardIndex]
+        {
+            get { return (Card)List[cardIndex]; }
+            set { List[cardIndex] = value; }
+        }
+
+        public void CopyTo(Cards targetCards)
+        {
+            for (int index = 0; index < this.Count; index++)
+            {
+                targetCards[index] = this[index];
+            }
+        }
+
+        public bool Contains(Card card) => InnerList.Contains(card);
+    }
+}
